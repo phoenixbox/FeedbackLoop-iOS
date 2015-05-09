c7502abad5eb26237c56f9bfcf61dfce3eed70e0
Shane Rogers <rogerssh@tcd.ie>
2015-05-09 11:18:06 -0700

    Add connection error message view

---
+++ FeedbackLoop/FBLConnectionErrorBGView.h
@@ -0,0 +1,24 @@
+//
+//  FBLConnectionErrorEmptyMessageView.h
+//  FeedbackLoop
+//
+//  Created by Shane Rogers on 5/9/15.
+//  Copyright (c) 2015 FBL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+extern NSString* const kConnectionErrorBGView;
+
+@interface FBLConnectionErrorBGView : UIView
+@property (weak, nonatomic) IBOutlet UIView *contentView;
+@property (weak, nonatomic) IBOutlet UILabel *title;
+@property (weak, nonatomic) IBOutlet UILabel *message;
+@property (weak, nonatomic) IBOutlet UIImageView *chatty;
+@property (weak, nonatomic) IBOutlet UIView *cloudContainer;
+
+@property (weak, nonatomic) IBOutlet UIImageView *rightCloud;
+@property (weak, nonatomic) IBOutlet UIImageView *middleCloud;
+@property (weak, nonatomic) IBOutlet UIImageView *leftCloud;
+
+@end
---
+++ FeedbackLoop/FBLConnectionErrorBGView.m
@@ -0,0 +1,23 @@
+//
+//  FBLConnectionErrorEmptyMessageView.m
+//  FeedbackLoop
+//
+//  Created by Shane Rogers on 5/9/15.
+//  Copyright (c) 2015 FBL. All rights reserved.
+//
+
+#import "FBLConnectionErrorBGView.h"
+
+NSString *const kConnectionErrorBGView = @"FBLConnectionErrorBGView";
+
+@implementation FBLConnectionErrorBGView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
