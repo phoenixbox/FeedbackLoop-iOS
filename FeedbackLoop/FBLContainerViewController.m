6b021c9eac44ca2e04f35a36ec192a62266f2c20
Shane Rogers <rogerssh@tcd.ie>
2015-05-09 18:42:27 -0700

    Remove container view cruft

--- FeedbackLoop/FBLContainerViewController.m
+++ FeedbackLoop/FBLContainerViewController.m
@@ -113,22 +113,13 @@
                                  } completion:nil];
 }
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (IBAction)closeWindow:(id)sender {
     [_chatViewController hideFeedbackLoopWindow];
 }
 
-- (void) dealloc{
+#pragma mark - Notification Center
+
+- (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
