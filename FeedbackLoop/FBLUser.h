bfc803c3e27eb638d9094b2dc708848f60a3d500
Shane Rogers <rogerssh@tcd.ie>
2015-05-24 18:15:22 -0700

    Update the FeedbackLoop title assets

---
+++ FeedbackLoop/FBLUser.h
@@ -0,0 +1,13 @@
+//
+//  FBLUser.h
+//  FeedbackLoop
+//
+//  Created by Shane Rogers on 5/24/15.
+//  Copyright (c) 2015 FBL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBLUser : NSObject
+
+@end
---
+++ FeedbackLoop/FBLUser.m
@@ -0,0 +1,13 @@
+//
+//  FBLUser.m
+//  FeedbackLoop
+//
+//  Created by Shane Rogers on 5/24/15.
+//  Copyright (c) 2015 FBL. All rights reserved.
+//
+
+#import "FBLUser.h"
+
+@implementation FBLUser
+
+@end
